8412b70e5febf345e54f156926cefd112762f47c
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-06-22 22:25:20 -0400

    Added query for five day forecast and used days space 8 indices apart on list array for five separate days to update 5 day forecast buttons

--- script.js
+++ script.js
@@ -35,6 +35,26 @@ $("#searchBtn").on("click", function (event) {
     }).then(function (response) {
         console.log(fiveQueryURL);
         console.log(response);
+        let dayOne = response.list[4];
+        let dayTwo = response.list[12];
+        let dayThree = response.list[20];
+        let dayFour = response.list[28];
+        let dayFive = response.list[36];
+        $("#day1").text(dayOne.dt_txt);
+        $("#day1Temp").text("Temperature: " + (1.8 * (dayOne.main.temp - 273) + 32).toFixed(1) + "F");
+        $("#day1Humidity").text("Humidity: " + dayOne.main.humidity + "%");
+        $("#day2").text(dayTwo.dt_txt);
+        $("#day2Temp").text("Temperature: " + (1.8 * (dayTwo.main.temp - 273) + 32).toFixed(1) + "F");
+        $("#day2Humidity").text("Humidity: " + dayTwo.main.humidity + "%");
+        $("#day3").text(dayThree.dt_txt);
+        $("#day3Temp").text("Temperature: " + (1.8 * (dayThree.main.temp - 273) + 32).toFixed(1) + "F");
+        $("#day3Humidity").text("Humidity: " + dayThree.main.humidity + "%");
+        $("#day4").text(dayFour.dt_txt);
+        $("#day4Temp").text("Temperature: " + (1.8 * (dayFour.main.temp - 273) + 32).toFixed(1) + "F");
+        $("#day4Humidity").text("Humidity: " + dayFour.main.humidity + "%");
+        $("#day5").text(dayFive.dt_txt);
+        $("#day5Temp").text("Temperature: " + (1.8 * (dayFive.main.temp - 273) + 32).toFixed(1) + "F");
+        $("#day5Humidity").text("Humidity: " + dayFive.main.humidity + "%");
     });
 
 });
